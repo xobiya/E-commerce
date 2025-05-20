cb07a080cabf5a8f4053001789c6dca7d286f1d7
xobiya <eshetufeleke21@gmail.com>
2025-05-20 22:24:39 +0300

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
 import Navbar from "./Sections/Navbar"
 import Sidebar from "./Sections/Sidebar"
 import Hero from "./Sections/Hero"
+import ProductList from "./Sections/ProductList"
+import Category from "./Sections/Category"
+import Selling from "./Sections/Selling"
+import Main from "./Sections/Main"
+import ProductItems from "./Sections/ProductItems"
 function App() {
 
   return (
@@ -8,6 +13,11 @@ function App() {
      <Navbar />
      <Sidebar />
      <Hero />
+     <ProductList />
+     <Category />
+     <Selling />
+     <Main />
+     <ProductItems />
     </>
   )
 }
