4b5cd89c06f4a72e73d204c8444e320b05c2e890
xobiya <eshetufeleke21@gmail.com>
2025-05-25 21:13:49 +0300

    Refactor Category component layout and styling for improved readability

--- src/Sections/Category.jsx
+++ src/Sections/Category.jsx
@@ -1,27 +1,31 @@
-import CategoryCard from '../Components/CategoryCard'
-import {categoryItems} from '../Components/index'
+import CategoryCard from '../Components/CategoryCard';
+import { categoryItems } from '../Components/index';
 
 const Category = () => {
   return (
-    <section className='mt-28 ml-[160px] mr-[80px]'>
-        <div className="flex-1 flex flex-col justify-start">
+    <section className="mt-28 px-4 sm:px-8 md:px-16 lg:px-32">
+      <div className="flex-1 flex flex-col justify-start">
         <div className="flex items-center mb-2">
           <span className="w-4 h-8 bg-red-500 rounded mr-2"></span>
-          <span className="text-red-500 font-semibold text-lg font['Poppins']"> Categories</span>
+          <span className="text-red-500 font-semibold text-lg font-poppins">
+            Categories
+          </span>
         </div>
-        </div>
-        <h1 className='text-zinc-950 text-5xl font-["inter"] ' >Browse By Category</h1>
-        <div className='flex flex-row gap-8 mt-15 justify-center'>
-            {
-                categoryItems.map((item)=>(
-                    <CategoryCard Item={item} key={item.id} />
-                ))
-            }
+      </div>
 
-        </div>
-<hr className='mt-20 bg-[#746d6d]  '></hr>
+      <h1 className="text-zinc-950 text-3xl sm:text-4xl md:text-5xl font-inter mb-8">
+        Browse By Category
+      </h1>
+
+      <div className="flex flex-wrap justify-center gap-6 md:gap-8">
+        {categoryItems.map((item) => (
+          <CategoryCard Item={item} key={item.id} />
+        ))}
+      </div>
+
+      <hr className="mt-20 bg-[#746d6d] border-0 h-[1px]" />
     </section>
-  )
-}
+  );
+};
 
-export default Category
+export default Category;
